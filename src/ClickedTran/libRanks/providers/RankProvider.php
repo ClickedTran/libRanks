f9fdaed26ef12bd537008a5c63325d733ef37f0d
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-12-04 17:18:10 +0700

    Upload RankProvider.php

---
+++ src/ClickedTran/libRanks/providers/RankProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ClickedTran\libRanks\providers;
+
+use pocketmine\player\Player;
+
+abstract class RankProvider
+{
+    public static function checkDependencies(): bool
+    {
+        return true;
+    }
+
+
+    abstract function getRank(Player $player, ?callable $callback = null);
+
+    abstract function giveRank(Player $player, string $rank, ?callable $callback = null);
+}
