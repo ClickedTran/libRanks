b87cdca224e95862a4f0a0c4f4475fab464e591a
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-12-11 21:59:43 +0700

    Update RankProvider.php

--- src/ClickedTran/libRanks/providers/RankProvider.php
+++ src/ClickedTran/libRanks/providers/RankProvider.php
@@ -11,13 +11,17 @@ abstract class RankProvider
         return true;
     }
 
+    abstract function getRank(Player|string $player, ?callable $callback = null);
 
-    abstract function getRank(Player $player, ?callable $callback = null);
-
-    abstract function giveRank(Player $player, string $rank, ?callable $callback = null);
+    abstract function giveRank(Player|string $player, string $rank, ?callable $callback = null);
     
     abstract function getRankData(string $rank, ?callable $callback = null);
-
-    abstract function removeRank(Player $player, string $rank, ?callable $callback = null);
-   
+    
+    abstract function removeRank(Player|string $player, string $rank, ?callable $callback = null);
+    
+    abstract function getPlayerPermission(Player|string $player, ?callable $callback = null);
+    
+    abstract function setPlayerPermission(Player|string $player, string $permission, ?callable $callback = null);
+    
+    abstract function unsetPlayerPermission(Player|string $player, string $permission, ?callable $callback = null);
 }
